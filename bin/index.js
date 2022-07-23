e2928ccb5fc5f2e4b44c5b48624d5dd8b2a78ac2
xuqingshan <xuqingshan@tianyancha.com>
2022-07-23 19:52:26 +0800

    feat: add lerna

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,4 @@
 #! /usr/bin/env node
-
-console.log('my cli. hello world....')
+const lib = require('clitest-hill-lib')
+console.log(lib.sum(1,2))
+console.log('my cli. hello world....test my local000000.')
---
+++ docs/lerna.md
@@ -0,0 +1,9 @@
+# lerna的使用：
+
+解决多个包的管理优化
+
+## lerna的使用：
+
+1. lerna init
+
+2. lerna create <package name>
